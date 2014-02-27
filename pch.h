319ce40f2acf347d7d78e9ece4cf08f566e05a4b
Hyeon Kim <simnalamburt@gmail.com>
2014-02-28 08:57:55 +0900

    Enabling Visual Style

--- pch.h
+++ pch.h
@@ -1,5 +1,10 @@
 #pragma once
 
+// Linker directive
+#pragma comment(linker,"\"/manifestdependency:type='win32' " \
+    "name='Microsoft.Windows.Common-Controls' version='6.0.0.0' " \
+    "processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
+
 // windows Çì´õ
 #include "targetver.h"
 #define WIN32_LEAN_AND_MEAN
