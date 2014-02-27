6aa9a9574b672e0a6e1c3a283c76d888da87f226
Hyeon Kim <simnalamburt@gmail.com>
2014-02-28 08:54:40 +0900

    빈 윈도우 띄우는 코드 작성

--- main.cpp
+++ main.cpp
@@ -1,8 +1,83 @@
 #include "pch.h"
+#include "ErrorReporter.h"
 
 
 
-int WINAPI wWinMain(HINSTANCE Instance, HINSTANCE, PWSTR, int Show)
+HINSTANCE InstanceHandle;
+
+LPCWSTR ClassName = L"TOPMOSTER";
+
+HWND WindowHandle;
+LPCWSTR WindowTitle = L"Topmoster";
+LONG WindowWidth = 100;
+LONG WindowHeight = 200;
+LRESULT CALLBACK WindowProcedure(HWND, UINT, WPARAM, LPARAM);
+
+
+
+
+
+int WINAPI wWinMain(HINSTANCE InstanceHandle, HINSTANCE, PWSTR, int ShowCommand)
 {
+    ::InstanceHandle = InstanceHandle;
 
+    //// Register a window class for subsequent use in calls to the CreateWindow
+    WNDCLASSEX wcex = {};
+    wcex.style = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc = WindowProcedure;
+    wcex.lpszClassName = ClassName;
+    wcex.hInstance = InstanceHandle;
+    wcex.hIcon = wcex.hIconSm = LoadIcon(NULL, IDI_WINLOGO);
+    wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wcex.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
+    wcex.cbSize = sizeof(wcex);
+    FALSE_ERROR(RegisterClassEx(&wcex));
+
+
+    //// Create a window
+    DWORD windowStyle = WS_SYSMENU | WS_MINIMIZEBOX | WS_CAPTION;
+    // Calculate desired size of the window
+    RECT emptyRect = { 0, 0, WindowWidth, WindowHeight };
+    FALSE_WARNING(AdjustWindowRect(&emptyRect, windowStyle, FALSE));
+    // Create the window
+    FALSE_ERROR(WindowHandle = CreateWindow(ClassName, WindowTitle, windowStyle, CW_USEDEFAULT, CW_USEDEFAULT,
+        emptyRect.right - emptyRect.left, emptyRect.bottom - emptyRect.top, nullptr, nullptr, InstanceHandle, nullptr));
+
+
+    //// Run
+    ShowWindow(WindowHandle, ShowCommand);
+    FALSE_WARNING(SetForegroundWindow(WindowHandle));
+    FALSE_WARNING(SetFocus(WindowHandle));
+
+    MSG message;
+    BOOL result;
+    while ((result = GetMessage(&message, NULL, 0, 0)) != 0)
+    {
+        FALSE_ERROR(result != -1);
+        TranslateMessage(&message);
+        DispatchMessage(&message);
+    }
+
+
+    //// Cleanup
+    if (InstanceHandle)
+    {
+        FALSE_WARNING(UnregisterClass(ClassName, InstanceHandle));
+    }
+
+    return message.wParam;
+}
+
+
+
+LRESULT CALLBACK WindowProcedure(HWND WindowHandle, UINT Message, WPARAM wParam, LPARAM lParam)
+{
+    switch (Message)
+    {
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        return 0;
+    default:
+        return DefWindowProc(WindowHandle, Message, wParam, lParam);
+    }
 }
